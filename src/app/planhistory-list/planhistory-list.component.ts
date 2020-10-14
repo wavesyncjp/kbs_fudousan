dbd5189646090b3ebc8a50e7fc60763da240579a
hirano-tsuyoshi <hirano@wavesync.co.jp>
2020-10-14 21:14:58 +0900

    hirano_10.14

--- src/app/planhistory-list/planhistory-list.component.ts
+++ src/app/planhistory-list/planhistory-list.component.ts
@@ -52,7 +52,8 @@ export class PlanHistoryListComponent extends BaseComponent {
   // tslint:disable-next-line:use-lifecycle-interface
   ngOnInit() {
     super.ngOnInit();
-    this.service.changeTitle('事業収支詳細');
+    //20201014_Add
+    this.service.changeTitle('事業収支履歴照会');
 
     const elementList = document.querySelectorAll('.detail-div');
     const element = elementList[0] as HTMLElement;
